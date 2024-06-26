986a6c105332c8700bfa8fe73bf0cc322af59568
dozikclozik <165407079+dozikclozik@users.noreply.github.com>
2024-06-27 00:49:52 +0300

    Update V.1.1.lua

--- V.1.1.lua
+++ V.1.1.lua
@@ -70,7 +70,7 @@ local function MakingDeathCube()
 		pcall(function()
 		local playerTouchedCube = game.Players:GetPlayerFromCharacter(hit.Parent)
 		if playerTouchedCube.Character:FindFirstChildOfClass("HumanoidRootPart")
-			and playerTouchedCube.Character:FindFirstChildOfClass("Tool") and playerTouchedCube ~= lplayer then
+			and playerTouchedCube.Character:FindFirstChildOfClass("Tool"):FindFirstChild("Handle") and playerTouchedCube ~= lplayer then
 			Hrp:PivotTo(playerTouchedCube.Character:FindFirstChild("HumanoidRootPart").CFrame
 				* CFrame.new(nil,nil, -6))
 			print(playerTouchedCube.Name .. " touched cube.")
